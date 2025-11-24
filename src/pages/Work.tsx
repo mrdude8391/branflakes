53135795e12c15ab4ce3baec222e8c57e918fd20
Bryan_w11 <h.bryan68@yahoo.ca>
2025-11-24 14:55:30 -0500

    moved info about myself into its own compoinent

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,20 +1,9 @@
-import githubLogo from "../assets/github.svg";
+import SelfInfo from "../components/SelfInfo";
 
 const Work = () => {
   return (
     <div className="container flex flex-col gap-8">
-      <div className="w-full p-2 border rounded-lg">
-        <div>
-          <h1>Bryan (Jr-Chee) Hu</h1>
-          <img src={githubLogo} className="github" alt="github logo" />
-        </div>
-
-        <sub>Toronto, Canada</sub>
-        <p className="mt-4">
-          I'm a 23 year old Full-Stack Developer. I have been programming for
-          over 4 years. I create professional websites and apps.
-        </p>
-      </div>
+      <SelfInfo />
       <div className="w-full ">
         <h1>What I work with</h1>
         <p className="mt-4">Blah</p>
