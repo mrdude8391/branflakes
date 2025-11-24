28526cf3ac205b0bb92d47448eb14c89405493e8
Bryan_w11 <h.bryan68@yahoo.ca>
2025-11-24 18:15:58 -0500

    added new compnents to work

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,9 +1,13 @@
+import Experience from "../components/Experience";
+import Portfolio from "../components/Portfolio";
 import SelfInfo from "../components/SelfInfo";
 
 const Work = () => {
   return (
-    <div className="container flex flex-col gap-8">
+    <div className="max-container justify-center flex flex-col sm:flex-row flex-wrap gap-8">
       <SelfInfo />
+      <Experience />
+      <Portfolio />
       <div className="w-full ">
         <h1>What I work with</h1>
         <p className="mt-4">Blah</p>
