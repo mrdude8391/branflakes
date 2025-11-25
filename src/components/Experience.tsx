30249d9bab2d9ad730576098c76b7197fbd829cf
Bryan_w11 <h.bryan68@yahoo.ca>
2025-11-25 17:33:35 -0500

    experience calass with work item elements cleaned up

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -2,33 +2,54 @@ import type { ReactNode } from "react";
 
 const Experience = () => {
   return (
-    <div className="dash-block">
+    <section className="dashboard-card flex flex-col gap-8">
       <h1>Experience</h1>
-      <WorkItem>
-        <Company>NOVX Systems</Company>
-      </WorkItem>
-    </div>
+      <div className="flex flex-col gap-12">
+        <WorkItem>
+          <Company>NOVX Systems</Company>
+          <Title>Junior Full Stack Developer (Co-op)</Title>
+          <Date>2021 - 2022</Date>
+          <Description>
+            Added and maintained features on .NET application and internal web
+            tooling in Angular. Made a fully functioning live chat service in
+            the .NET application. Created API endpoints and managed data using
+            Microsoft SQL.
+          </Description>
+        </WorkItem>
+
+        <WorkItem>
+          <Company>YMT Lion Dance</Company>
+          <Title>Full Stack Web Developer</Title>
+          <Date>2025</Date>
+          <Description>
+            Designed and developed a website for a local lion dance team. It
+            features an email based booking system, responsive UI, and
+            animations.
+          </Description>
+        </WorkItem>
+      </div>
+    </section>
   );
 };
 
 export default Experience;
 
 const WorkItem = ({ children }: { children: ReactNode }) => {
-  return (
-    <div>
-      {children}
-      <h3>Junior Full Stack Developer (Co-op)</h3>
-      <sub className="text-sm">2021 - 2022</sub>
-      <p>
-        Added and maintained features on .NET application and internal web
-        tooling in Angular. Made a fully functioning live chat service in the
-        .NET application. Created API endpoints and managed data using Microsoft
-        SQL.
-      </p>
-    </div>
-  );
+  return <div>{children}</div>;
 };
 
 const Company = ({ children }: { children: ReactNode }) => {
   return <h2>{children}</h2>;
+};
+
+const Title = ({ children }: { children: ReactNode }) => {
+  return <h3>{children}</h3>;
+};
+
+const Date = ({ children }: { children: ReactNode }) => {
+  return <p className="text-sm text-foreground-muted">{children}</p>;
+};
+
+const Description = ({ children }: { children: ReactNode }) => {
+  return <p className="mt-2 text-lg leading-6">{children}</p>;
 };
