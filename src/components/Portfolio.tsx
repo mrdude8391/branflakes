2636a4b85a7d3c508f8082329d287bcaa4e4acd1
Bryan_w11 <h.bryan68@yahoo.ca>
2025-12-03 13:12:29 -0500

    portfolio styling

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,11 +1,37 @@
+import { SquareArrowOutUpRight } from "lucide-react";
+import SportsDesktopImg from "../assets/Sports App/desktop.png";
+import ReactSVG from "../assets/React/ReactSVG";
+import { Link } from "react-router-dom";
+
 const Portfolio = () => {
   return (
-    <section className="dashboard-card">
+    <section className="dashboard-card flex flex-col gap-4">
       <h1>Portfolio</h1>
-      <div>
-        <div>
-          <img src="" alt="" />
-        </div>
+      <div className="my-work">
+        <Link to="/volley" viewTransition>
+          <div className="project-image-card group">
+            <img src={SportsDesktopImg} alt="Sports app desktop image" />
+
+            <div className="overlay group-hover:bottom-0 ">
+              <h3>Volleyball Tracker</h3>
+              <ul>
+                <li>
+                  <ReactSVG className="size-6 fill-foreground" />
+                  <p>React</p>
+                </li>
+                <li>
+                  <ReactSVG className="size-6 fill-foreground" />
+                  <p>React</p>
+                </li>
+              </ul>
+              <p>VOLLEYBALL STATS WEB APP</p>
+              <button>
+                <p>Learn More </p>
+                <SquareArrowOutUpRight className="size-3 mx-1" />
+              </button>
+            </div>
+          </div>
+        </Link>
       </div>
     </section>
   );
