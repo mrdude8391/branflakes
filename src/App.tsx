5ca7f7f2102c42c40962293b6eae8625f806dd79
Bryan_w11 <h.bryan68@yahoo.ca>
2025-12-03 13:06:20 -0500

    use create browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import { Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "./pages/layout/MainLayout";
 import About from "./pages/About";
 import Work from "./pages/Work";
 import NotFound from "./pages/NotFound";
+import VolleyApp from "./pages/portfolio/VolleyApp";
 
 function App() {
-  return (
-    <>
-      <Routes>
-        {/* Layout */}
-        <Route element={<MainLayout />}>
-          {/* Routes */}
-          <Route path="/" element={<Work />} />
-          <Route path="/about" element={<About />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </>
-  );
+  const router = createBrowserRouter([
+    {
+      element: <MainLayout />,
+      children: [
+        { path: "/", element: <Work /> },
+        { path: "/about", element: <About /> },
+        { path: "/volley", element: <VolleyApp /> },
+        { path: "*", element: <NotFound /> },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
