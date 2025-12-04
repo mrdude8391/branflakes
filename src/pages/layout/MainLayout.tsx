ced8a7a1009690569808203d42b97972823c7262
Bryan_w11 <h.bryan68@yahoo.ca>
2025-12-04 14:45:41 -0500

    moved nav bar

--- src/pages/layout/MainLayout.tsx
+++ src/pages/layout/MainLayout.tsx
@@ -1,12 +1,10 @@
 import Footer from "../../components/Footer";
-import Nav from "../../components/Nav";
 import { Outlet } from "react-router-dom";
 import ScrollToTop from "../../components/ScrollToTop";
 
 const MainLayout = () => {
   return (
     <>
-      <Nav />
       <div id="layout-outlet">
         <ScrollToTop />
         <Outlet />
