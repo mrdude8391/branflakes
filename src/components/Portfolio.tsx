0b015c4f2ae1754244552fef7474d4fb7a279602
Bryan_w11 <h.bryan68@yahoo.ca>
2025-12-08 14:47:11 -0500

    styled projects component for multiple projects

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,40 +1,65 @@
 import { SquareArrowOutUpRight } from "lucide-react";
 import SportsDesktopImg from "/src/assets/sports_app_screenshots/vb - athlete stat.png";
-import { ReactSVG, MongoDBSVG } from "../assets/SvgLogos";
+import {
+  ReactSVG,
+  MongoDBSVG,
+  ExpressSVG,
+  NodeJSSVG,
+} from "../assets/SvgLogos";
 import { Link } from "react-router-dom";
 
 const Portfolio = () => {
   return (
     <section className="dashboard-card flex flex-col gap-4">
       <h1>Projects</h1>
-      <div className="my-work">
-        <Link to="/volley">
-          <div className="project-image-card group">
-            <img src={SportsDesktopImg} alt="Sports app desktop image" />
+      <div className="projects">
+        <div className="project-container">
+          <VbAppCard />
+        </div>
 
-            <div className="overlay group-hover:bottom-0 ">
-              <h3>Volleyball Tracker</h3>
-              <ul>
-                <li>
-                  <ReactSVG className="size-6 fill-foreground" />
-                  <p>React</p>
-                </li>
-                <li>
-                  <MongoDBSVG className="size-6 fill-foreground" />
-                  <p>MongoDB</p>
-                </li>
-              </ul>
-              <p>VOLLEYBALL STATS WEB APP</p>
-              <button>
-                <p>Learn More </p>
-                <SquareArrowOutUpRight className="size-3 mx-1" />
-              </button>
-            </div>
-          </div>
-        </Link>
+        <div className="project-container">
+          <VbAppCard />
+        </div>
       </div>
     </section>
   );
 };
 
 export default Portfolio;
+
+const VbAppCard = () => {
+  return (
+    <Link to="/volley">
+      <div className="project-image-card group">
+        <img src={SportsDesktopImg} alt="Sports app desktop image" />
+
+        <div className="overlay group-hover:bottom-0 ">
+          <h3>Volleyball Tracker</h3>
+          <ul>
+            <li>
+              <MongoDBSVG className="size-6 fill-foreground" />
+              <p>MongoDB</p>
+            </li>
+            <li>
+              <ExpressSVG className="size-6 fill-foreground" />
+              <p>Express</p>
+            </li>
+            <li>
+              <ReactSVG className="size-6 fill-foreground" />
+              <p>React</p>
+            </li>
+            <li>
+              <NodeJSSVG className="size-6 fill-foreground" />
+              <p>Node.js</p>
+            </li>
+          </ul>
+          <p>VOLLEYBALL STATS WEB APP</p>
+          <button>
+            <p>Learn More </p>
+            <SquareArrowOutUpRight className="size-3 mx-1" />
+          </button>
+        </div>
+      </div>
+    </Link>
+  );
+};
