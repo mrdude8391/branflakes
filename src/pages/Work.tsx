a5f4a334a4be6725a20653c7cced537163c80c14
Bryan_w11 <h.bryan68@yahoo.ca>
2025-12-08 14:46:56 -0500

    placed work dashboard area in its own div

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -5,11 +5,14 @@ import SelfInfo from "../components/SelfInfo";
 
 const Work = () => {
   return (
-    <div className="max-container justify-center flex flex-col items-center  flex-wrap gap-8">
+    <div className="max-container justify-center flex flex-col items-center flex-wrap gap-8">
       <Nav />
-      <SelfInfo />
-      <Experience />
-      <Portfolio />
+      <div className="dashboard">
+        <SelfInfo />
+        <Experience />
+        <Portfolio />
+      </div>
+
       <div className="w-full ">
         <h1>What I work with</h1>
         <p className="mt-4">Blah</p>
