8ca1d14710ce907d21a2e5bf4c867d481b94e03c
Bryan_w11 <h.bryan68@yahoo.ca>
2025-12-09 17:48:49 -0500

    dark mudkip blue

--- src/pages/portfolio/VolleyApp.tsx
+++ src/pages/portfolio/VolleyApp.tsx
@@ -27,7 +27,7 @@ const VolleyApp = () => {
             <li>Node</li>
           </ul>
 
-          <button className="bg-mudkip hover:bg-mudkip/80 text-background">
+          <button className="bg-mudkip hover:bg-mudkip/80">
             <a href="https://sports-data-app.vercel.app/" target="_blank">
               Visit App <SquareArrowUpRight className="size-4" />
             </a>
