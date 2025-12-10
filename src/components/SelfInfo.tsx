c0aab5b9a3a77f8d0a51df8b9c03e50d61dc2731
Bryan_w11 <h.bryan68@yahoo.ca>
2025-12-10 14:13:14 -0500

    styling fix

--- src/components/SelfInfo.tsx
+++ src/components/SelfInfo.tsx
@@ -8,8 +8,8 @@ import { GraduationCap, Languages, Layers, Mail, MapPin } from "lucide-react";
 
 const SelfInfo = () => {
   return (
-    <section className="dashboard-card flex flex-col gap-4">
-      <div>
+    <section className="self-info dashboard-card flex flex-col gap-4">
+      <div className="header">
         <h1 className="hover-pop origin-bottom-left">Bryan (Jr-Chee) Hu</h1>
         <p className="sub hover-pop origin-bottom-left">Software Engineer</p>
         <p className="mt-4 sm:text-xl hover-pop origin-bottom-left hover:scale-102">
@@ -17,59 +17,47 @@ const SelfInfo = () => {
           websites and web apps.
         </p>
       </div>
-      <div className="flex flex-wrap bg-background gap-2 px-4 py-2 rounded-lg text-foreground-muted fill-foreground-muted">
-        <div className="rounded-accent-container gap-1 ">
+      <div className="tag-container">
+        <div className="rounded-accent-tag ">
           <ReactSVG className="size-5" />
           <p>React</p>
         </div>
-        <div className="rounded-accent-container gap-1 ">
+        <div className="rounded-accent-tag">
           <TypescriptSVG className="size-5" />
           <p>Typescript</p>
         </div>
-        <div className="rounded-accent-container gap-1 ">
+        <div className="rounded-accent-tag">
           <Layers className="size-5" />
           <p>Fullstack</p>
         </div>
-        <div className="rounded-accent-container gap-1 ">
+        <div className="rounded-accent-tag">
           <MapPin className="size-5" />
           Toronto
         </div>
-        <div className="rounded-accent-container gap-1 ">
+        <div className="rounded-accent-tag">
           <GraduationCap className="size-5" />
           <p>Biomedical B.Eng</p>
         </div>
-        <div className="rounded-accent-container gap-1 ">
+        <div className="rounded-accent-tag">
           <Languages className="size-5" />
           English & Chinese
         </div>
       </div>
-      <div className="flex flex-wrap gap-2">
-        <a
-          href="mailto:jrcheehu@gmail.com"
-          target="_blank"
-          className="flex flex-auto"
-        >
-          <button className="social-button">
+      <div className="socials">
+        <a href="mailto:jrcheehu@gmail.com" target="_blank">
+          <button>
             <Mail className="size-5 lg:size-6" />
             Email
           </button>
         </a>
-        <a
-          href="https://github.com/mrdude8391"
-          target="_blank"
-          className="flex flex-auto"
-        >
-          <button className="social-button">
+        <a href="https://github.com/mrdude8391" target="_blank">
+          <button>
             <GithubSVG className="size-5 lg:size-6 fill-inherit" />
             GitHub
           </button>
         </a>
-        <a
-          href="https://www.linkedin.com/in/hu-bryan/"
-          target="_blank"
-          className="flex flex-auto"
-        >
-          <button className="social-button">
+        <a href="https://www.linkedin.com/in/hu-bryan/" target="_blank">
+          <button>
             <LinkedInSVG className="size-5 lg:size-6 fill-inherit" />
             LinkedIn
           </button>
