7d6bdecd18b885422babb2197227e6730ae42cfb
BH-PC <jrcheehu@gmail.com>
2026-09-23 15:50:17 -0400

    Implemented getJournalPosts() to retrieve all Journals as a list of objects. Refactored the component to display journal entires in a <ul/> element.

--- src/pages/journal/Journal.tsx
+++ src/pages/journal/Journal.tsx
@@ -1,22 +1,22 @@
 import Markdown from "markdown-to-jsx/react";
-import markdownContent from "../../posts/journal/2026-09-22-first-journal-post.md?raw";
 import SyntaxHighlightedCode from "../../components/SyntaxHighlightedCode";
 import "github-markdown-css/github-markdown.css";
-import matter from "gray-matter";
+import { getJournalPosts } from "../../lib/journal.ts";
 
 const Journal = () => {
-  const matterResult = matter(markdownContent);
-  const { content, data } = matterResult;
+  const journals = getJournalPosts();
   return (
     <section className="dashboard-card flex flex-col gap-12 ">
       <ul className="journals markdown-body">
-        <li className="journal">
-          <h1>{data.title}</h1>
-          <p>{data.date}</p>
-          <Markdown options={{ overrides: { code: SyntaxHighlightedCode } }}>
-            {content}
-          </Markdown>
-        </li>
+        {journals.map((journal) => (
+          <li key={journal.id} className="journal">
+            <h1>{journal.title}</h1>
+            <p>{journal.date}</p>
+            <Markdown options={{ overrides: { code: SyntaxHighlightedCode } }}>
+              {journal.content}
+            </Markdown>
+          </li>
+        ))}
       </ul>
     </section>
   );
