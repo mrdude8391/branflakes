aaf225aca4bfcb8445b86a268bc95c137ce2d44a
BH-PC <jrcheehu@gmail.com>
2026-09-22 20:19:01 -0400

    Added Markdown rendering component

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,6 +1,9 @@
 import type { ReactNode } from "react";
+import Markdown from "react-markdown";
 
 const Experience = () => {
+  const markdown = "# Hi, *Pluto*!";
+
   return (
     <section className="dashboard-card flex flex-col gap-8">
       <h1 className="hover-pop origin-bottom-left">Experience</h1>
@@ -27,6 +30,8 @@ const Experience = () => {
             animations.
           </Description>
         </WorkItem>
+
+        <Markdown>{markdown}</Markdown>
       </div>
     </section>
   );
