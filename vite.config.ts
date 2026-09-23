1134725d07607ba2f92148dc64115b89638ea307
BH-PC <jrcheehu@gmail.com>
2026-09-23 15:47:13 -0400

    added  buffer for gray matter

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,8 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    nodePolyfills({ globals: { Buffer: true } }),
+    nodePolyfills({
+      globals: { Buffer: true },
+    }),
   ],
 });
