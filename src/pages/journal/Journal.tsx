5c34786539bbc2649113d7919505626f6a94016b
BH-PC <jrcheehu@gmail.com>
2026-09-23 14:39:30 -0400

    Fix bug with gray-matter and vite requireing polyfills.

--- src/pages/journal/Journal.tsx
+++ src/pages/journal/Journal.tsx
@@ -2,8 +2,12 @@ import Markdown from "markdown-to-jsx/react";
 import markdownContent from "../../posts/journal/2026-09-22-first-journal-post.md?raw";
 import SyntaxHighlightedCode from "../../components/SyntaxHighlightedCode";
 import "github-markdown-css/github-markdown.css";
+import matter from "gray-matter";
 
 const Journal = () => {
+  const matterResult = matter(markdownContent);
+  const { content, data } = matterResult;
+  const { title, date } = data;
   return (
     <section className="dashboard-card flex flex-col gap-12 ">
       <ul className="journals markdown-body">
@@ -11,7 +15,7 @@ const Journal = () => {
           <p>{title}</p>
           <p>{date}</p>
           <Markdown options={{ overrides: { code: SyntaxHighlightedCode } }}>
-            {markdownContent}
+            {content}
           </Markdown>
         </li>
         <li className="journal">
