ca7a4dce9f26f8d3876959a2d9cd9885a0ca721b
BH-PC <jrcheehu@gmail.com>
2026-09-23 13:59:40 -0400

    Reformat HTML Hierarchy

--- src/pages/journal/Journal.tsx
+++ src/pages/journal/Journal.tsx
@@ -1,17 +1,25 @@
 import Markdown from "markdown-to-jsx/react";
 import markdownContent from "../../posts/journal/2026-09-22-first-journal-post.md?raw";
 import SyntaxHighlightedCode from "../../components/SyntaxHighlightedCode";
+import "github-markdown-css/github-markdown.css";
 
 const Journal = () => {
   return (
-    <section className="dashboard-card flex flex-col gap-8 markdown-body">
-      <div className="flex flex-col gap-12">
-        <div>
+    <section className="dashboard-card flex flex-col gap-12 ">
+      <ul className="journals markdown-body">
+        <li className="journal">
+          <p>{title}</p>
+          <p>{date}</p>
           <Markdown options={{ overrides: { code: SyntaxHighlightedCode } }}>
             {markdownContent}
           </Markdown>
-        </div>
-      </div>
+        </li>
+        <li className="journal">
+          <Markdown options={{ overrides: { code: SyntaxHighlightedCode } }}>
+            {markdownContent}
+          </Markdown>
+        </li>
+      </ul>
     </section>
   );
 };
